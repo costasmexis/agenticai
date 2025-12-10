9bdb7c4dd3339c266cd75c4b7d0c361b0cd7db50
costasmexis <costasmexis@gmail.com>
2025-12-10 14:46:28 +0100

    Update

--- src/meltingpoint.py
+++ src/meltingpoint.py
@@ -6,16 +6,14 @@ import gradio as gr
 
 load_dotenv(override=True) # override=True means any existing environment variables will be overwritten by values from the .env file.
 
-ollama = OpenAI(
-    api_key=os.getenv("GEMINI_API_KEY"),
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai"
-)
+ollama = OpenAI(api_key=os.getenv("GEMINI_API_KEY"), base_url="https://generativelanguage.googleapis.com/v1beta/openai")
 
 system_prompt = "You are a professional computational chemist specializing in molecular property prediction. \
 Your task is to predict the melting point (in Kelvin) of a molecule given its SMILES string. \
 Input: The user will provide a single SMILES string. \
 Output: You must return only one number, representing the predicted melting point in Kelvin, with no explanation, no units, and no additional text. \
-If you are unsure, output your best estimate as a number. Do not include comments, symbols, units, or text."
+Feel free to seach internet for the metling point of the compound. \
+If you are unsure, output your best estimate as a number. Do not include comments, symbols, units, or text. "
 
 def chat(message, history):
     messages = [{"role": "system", "content": system_prompt}] + history + [{"role": "user", "content": message}]
